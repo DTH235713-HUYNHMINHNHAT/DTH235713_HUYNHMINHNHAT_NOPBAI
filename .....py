4571bd7535842aba913a0d94c11ee21717cf1f91
DTH235713_HUYNHMINHNHAT <nhat_dth235713@student.agu.edu.vn>
2025-08-26 09:07:02 +0700

    Add files via upload

---
+++ .....py
@@ -0,0 +1,10 @@
+#chuong trinh tinh tong hai so
+#nhap so
+so1 = int(input("Nhap So Thu Nhat: "));
+so2 = int(input("Nhap So Thu Hai: "));
+
+#tinh tong
+tong = so1 + so2;
+
+#hien thi ket qua
+print("Tong 2 so =  ",so1 ,"+" , so2,"=",tong);
